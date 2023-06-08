1774171070cbc77882fef570f1499536ea3ea698
hongyonghan <418209696@qq.com>
2023-06-08 11:03:35 +0800

    update

---
+++ NIC_driver_kernel_5.4/drivers/driver/rtl8812AU_linux_v5.9.3.2_37279.20201012/os_dep/linux/.rtw_rhashtable.o.cmd
@@ -0,0 +1,25 @@
+cmd_/home/cocolab/drivers/driver/rtl8812AU_linux_v5.9.3.2_37279.20201012/os_dep/linux/rtw_rhashtable.o := gcc -Wp,-MD,/home/cocolab/drivers/driver/rtl8812AU_linux_v5.9.3.2_37279.20201012/os_dep/linux/.rtw_rhashtable.o.d  -nostdinc -isystem /usr/lib/gcc/x86_64-linux-gnu/9/include  -I./arch/x86/include -I./arch/x86/include/generated  -I./include -I./arch/x86/include/uapi -I./arch/x86/include/generated/uapi -I./include/uapi -I./include/generated/uapi -include ./include/linux/kconfig.h -Iubuntu/include  -include ./include/linux/compiler_types.h -D__KERNEL__ -Wall -Wundef -Werror=strict-prototypes -Wno-trigraphs -fno-strict-aliasing -fno-common -fshort-wchar -fno-PIE -Werror=implicit-function-declaration -Werror=implicit-int -Werror=return-type -Wno-format-security -std=gnu89 -mno-sse -mno-mmx -mno-sse2 -mno-3dnow -mno-avx -fcf-protection=none -m64 -falign-jumps=1 -falign-loops=1 -mno-80387 -mno-fp-ret-in-387 -mpreferred-stack-boundary=3 -mskip-rax-setup -mtune=generic -mno-red-zone -mcmodel=kernel -DCONFIG_X86_X32_ABI -DCONFIG_AS_CFI=1 -DCONFIG_AS_CFI_SIGNAL_FRAME=1 -DCONFIG_AS_CFI_SECTIONS=1 -DCONFIG_AS_SSSE3=1 -DCONFIG_AS_AVX=1 -DCONFIG_AS_AVX2=1 -DCONFIG_AS_AVX512=1 -DCONFIG_AS_SHA1_NI=1 -DCONFIG_AS_SHA256_NI=1 -Wno-sign-compare -fno-asynchronous-unwind-tables -mindirect-branch=thunk-extern -mindirect-branch-register -fno-jump-tables -fno-delete-null-pointer-checks -Wno-frame-address -Wno-format-truncation -Wno-format-overflow -Wno-address-of-packed-member -O2 --param=allow-store-data-races=0 -Wframe-larger-than=1024 -fstack-protector-strong -Wimplicit-fallthrough -Wno-unused-but-set-variable -Wno-unused-const-variable -fno-omit-frame-pointer -fno-optimize-sibling-calls -fno-var-tracking-assignments -pg -mrecord-mcount -mfentry -DCC_USING_FENTRY -flive-patching=inline-clone -Wdeclaration-after-statement -Wvla -Wno-pointer-sign -Wno-stringop-truncation -Wno-array-bounds -Wno-stringop-overflow -Wno-restrict -Wno-maybe-uninitialized -fno-strict-overflow -fno-merge-all-constants -fmerge-constants -fno-stack-check -fconserve-stack -Werror=date-time -Werror=incompatible-pointer-types -Werror=designated-init -fmacro-prefix-map=./= -Wno-packed-not-aligned -O1 -Wno-unused-variable -Wno-date-time -I/home/cocolab/drivers/driver/rtl8812AU_linux_v5.9.3.2_37279.20201012/include -I/home/cocolab/drivers/driver/rtl8812AU_linux_v5.9.3.2_37279.20201012/platform -I/home/cocolab/drivers/driver/rtl8812AU_linux_v5.9.3.2_37279.20201012/hal/btc -DCONFIG_RTL8812A -DCONFIG_MP_INCLUDED -DCONFIG_POWER_SAVING -DCONFIG_EFUSE_CONFIG_FILE -DEFUSE_MAP_PATH=\"/system/etc/wifi/wifi_efuse_8812au.map\" -DWIFIMAC_PATH=\"/data/wifimac.txt\" -DCONFIG_LOAD_PHY_PARA_FROM_FILE -DREALTEK_CONFIG_PATH=\"/lib/firmware/\" -DCONFIG_TXPWR_BY_RATE=1 -DCONFIG_TXPWR_BY_RATE_EN=0 -DCONFIG_TXPWR_LIMIT=1 -DCONFIG_TXPWR_LIMIT_EN=0 -DCONFIG_RTW_ADAPTIVITY_EN=0 -DCONFIG_RTW_ADAPTIVITY_MODE=0 -DCONFIG_IEEE80211W -DHIGH_ACTIVE_HST2DEV=0 -DCONFIG_BR_EXT '-DCONFIG_BR_EXT_BRNAME="'br0'"' -DCONFIG_RTW_NAPI -DCONFIG_RTW_GRO -DCONFIG_RTW_NETIF_SG -DCONFIG_RTW_DEBUG -DRTW_LOG_LEVEL=4 -DCONFIG_PROC_DEBUG -DCONFIG_RTW_UP_MAPPING_RULE=0 -DDM_ODM_SUPPORT_TYPE=0x04 -DCONFIG_LITTLE_ENDIAN -DCONFIG_IOCTL_CFG80211 -DRTW_USE_CFG80211_STA_EVENT -I/home/cocolab/drivers/driver/rtl8812AU_linux_v5.9.3.2_37279.20201012/hal/phydm -I/home/cocolab/drivers/driver/rtl8812AU_linux_v5.9.3.2_37279.20201012/core/crypto  -DMODULE  -DKBUILD_BASENAME='"rtw_rhashtable"' -DKBUILD_MODNAME='"8812au"' -c -o /home/cocolab/drivers/driver/rtl8812AU_linux_v5.9.3.2_37279.20201012/os_dep/linux/rtw_rhashtable.o /home/cocolab/drivers/driver/rtl8812AU_linux_v5.9.3.2_37279.20201012/os_dep/linux/rtw_rhashtable.c
+
+source_/home/cocolab/drivers/driver/rtl8812AU_linux_v5.9.3.2_37279.20201012/os_dep/linux/rtw_rhashtable.o := /home/cocolab/drivers/driver/rtl8812AU_linux_v5.9.3.2_37279.20201012/os_dep/linux/rtw_rhashtable.c
+
+deps_/home/cocolab/drivers/driver/rtl8812AU_linux_v5.9.3.2_37279.20201012/os_dep/linux/rtw_rhashtable.o := \
+    $(wildcard include/config/rtw/mesh.h) \
+    $(wildcard include/config/mmu.h) \
+  include/linux/kconfig.h \
+    $(wildcard include/config/cpu/big/endian.h) \
+    $(wildcard include/config/booger.h) \
+    $(wildcard include/config/foo.h) \
+  include/linux/compiler_types.h \
+    $(wildcard include/config/have/arch/compiler/h.h) \
+    $(wildcard include/config/enable/must/check.h) \
+    $(wildcard include/config/optimize/inlining.h) \
+    $(wildcard include/config/cc/has/asm/inline.h) \
+  include/linux/compiler_attributes.h \
+  include/linux/compiler-gcc.h \
+    $(wildcard include/config/arm64.h) \
+    $(wildcard include/config/retpoline.h) \
+    $(wildcard include/config/arch/use/builtin/bswap.h) \
+
+/home/cocolab/drivers/driver/rtl8812AU_linux_v5.9.3.2_37279.20201012/os_dep/linux/rtw_rhashtable.o: $(deps_/home/cocolab/drivers/driver/rtl8812AU_linux_v5.9.3.2_37279.20201012/os_dep/linux/rtw_rhashtable.o)
+
+$(deps_/home/cocolab/drivers/driver/rtl8812AU_linux_v5.9.3.2_37279.20201012/os_dep/linux/rtw_rhashtable.o):
